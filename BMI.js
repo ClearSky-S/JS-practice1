992e3913101ccc85145d7c7f631dcd45fea9b231
2021038131 <ggogono2@gmail.com>
2022-03-08 11:47:33 +0900

    bmi

--- BMI.js
+++ BMI.js
@@ -3,7 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         const h = parseFloat(document.getElementById('height').value);
         const w = parseFloat(document.getElementById('weight').value);
-        document.getElementById('bmi').innerHTML = (w/h/h).toFixed(3);
+        if(!isNaN(w/h/h)){
+            document.getElementById('bmi').innerHTML = (w/h/h).toFixed(3);
+        } else {
+            alert('Wrong Data');
+            document.getElementById('bmi').innerHTML = 0;
+        }
         console.log(h,w,w/h/h);
     })
 });
